34ecdf1125b0323eb2a903781b9d80bafc3a1352
jo-kwsm <ytk.kwsm@gmail.com>
2020-11-28 01:00:57 +0900

    実行速度を表示

--- src/test.py
+++ src/test.py
@@ -1,10 +1,17 @@
 from plane import Plane
 
+import time
+
 def main():
-  plane=Plane("data/OAG.csv")
-  print("loaded.")
-  cnt = plane.density(judge_time=0, point=(0,0))
-  print(cnt)
+	start = time.time()
+	plane=Plane("data/OAG.csv")
+	mid = time.time()
+	print(mid-start, "seconds")
+	cnt = plane.density(judge_time=0, point=(0,0))
+	end = time.time()
+	print(end-mid, "seconds")
+	print(cnt)
+
 
 if __name__ == "__main__":
-  main()
+  	main()
