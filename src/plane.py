e2ed94fba60473428bd57070e8eb5b8b6efe7198
jo-kwsm <ytk.kwsm@gmail.com>
2020-12-23 00:27:40 +0900

    リファクタリング

--- src/plane.py
+++ src/plane.py
@@ -86,22 +86,30 @@ class Plane:
         return point
 
 
-    def get_point(self, dep_time, arr_time, judge_datetime, dep_airport, arr_airport):
+    def get_point(self, idx, judge_datetime):
         """
         飛行機一機の座標を求める。
         線形計算。
 
         Args:
-            dep_time(str):出発時刻
-            arr_time(str):到着時刻
+            idx(int):知りたいフライトのdataにおけるindex
             judge_datetime(datetime):位置を知りたい時刻
-            dep_airport(str):出発空港コード
-            arr_airport(str):到着空港コード
         Returns:
-            tuple(latitude:int, longitude:int):飛行機のjudge_timeにおける座標
+            tuple(latitude_deg:float, longitude_deg:float):飛行機のjudge_timeにおける座標
         Note:
             judge_time時に空中にいなければNoneを返す。
         """
+        # 運行期間外なら処理しない
+        ef = self.str2date(self.timetable["Effective From"].iloc[idx])
+        et = self.str2date(self.timetable["Effective To"].iloc[idx])
+        ef = datetime.datetime.combine(ef, datetime.time(0, 0))
+        et = datetime.datetime.combine(et, datetime.time(23, 59))
+        if judge_datetime < ef or et < judge_datetime:
+            return None
+
+        # 時刻情報を処理
+        dep_time = self.timetable["Local Dep Time"].iloc[idx]
+        arr_time = self.timetable["Local Arr Time"].iloc[idx]
         dep_datetime = self.int2datetime(dep_time)
         arr_datetime = self.int2datetime(arr_time)
 
@@ -114,8 +122,11 @@ class Plane:
         if judge_datetime <= dep_datetime or arr_datetime <= judge_datetime or arr_datetime <= dep_datetime:
             return None
 
+        dep_airport = self.timetable["Dep Airport Code"].iloc[idx]
+        arr_airport = self.timetable["Arr Airport Code"].iloc[idx]
         dep_point = self.airport2point[dep_airport]
         arr_point = self.airport2point[arr_airport]
+
         try:
             point = (
                 self.divide(
@@ -139,27 +150,20 @@ class Plane:
         return point
 
 
-    def dis_check(self, p1, p2):
+    def distance(self, p1, p2):
         """
-        2地点間の距離が閾値以下かどうかを判定
+        2地点間の距離を返す
 
         Args:
-        p1(tuple(int,int)):1つ目の地点の座標
-        p2(tuple(int,int)):2つ目の地点の座標
+        p1(tuple(float,float)):1つ目の地点の座標
+        p2(tuple(float,float)):2つ目の地点の座標
         Returns:
-        res(boolean):2地点間の距離が閾値以下かどうか
-        以下ならTrue
-        Note:
-        地点が片方でもNoneならFalseを返す
+        dis(float):2地点間の距離(km)
         """
-        if p1 is None or p2 is None:
-            res = False
-        else:
-            res = math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
-            res = math.radians(res)
-            res = self.raius*res
-            res = (res <= self.area)
-        return res
+        dis = math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+        dis = math.radians(dis)
+        dis = self.raius*dis
+        return dis
 
 
     def density(self, judge_datetime, point):
@@ -172,25 +176,13 @@ class Plane:
         Returns:
             plane_num(int):中心座標から一定距離内に存在する機体の数
         """
-
         plane_num = 0
 
-        for i in range(len(self)):
-            # 日付情報を処理
-            ef = self.str2date(self.timetable["Effective From"].iloc[i])
-            ef = datetime.datetime.combine(ef, datetime.time(0, 0))
-            et = self.str2date(self.timetable["Effective To"].iloc[i])
-            et = datetime.datetime.combine(et, datetime.time(23, 59))
-            if judge_datetime < ef or et < judge_datetime:
+        for idx in range(len(self)):
+            plane_point = self.get_point(idx, judge_datetime)
+            if plane_point is None:
                 continue
-
-            # 時刻情報を処理
-            dep_time = self.timetable["Local Dep Time"].iloc[i]
-            arr_time = self.timetable["Local Arr Time"].iloc[i]
-            dep_airport = self.timetable["Dep Airport Code"].iloc[i]
-            arr_airport = self.timetable["Arr Airport Code"].iloc[i]
-            
-            if self.dis_check(self.get_point(dep_time, arr_time, judge_datetime, dep_airport, arr_airport), point):
+            if self.distance(plane_point, point) <=self.area:
                 plane_num+=1
 
         return plane_num
