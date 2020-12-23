da0776cdeb8ca5a82ac6cdf127e78d7801f6794d
jo-kwsm <ytk.kwsm@gmail.com>
2020-12-24 00:19:09 +0900

    テストの実施

--- src/test.py
+++ src/test.py
@@ -8,8 +8,11 @@ def main():
 	plane=Plane("data/OAG.csv", "data/airports.csv")
 	mid = time.time()
 	print(mid-start, "seconds")
-	judge_datetime = datetime.datetime(2019, 5, 1, 12, 0)
-	cnt = plane.density(judge_datetime, point=(0,0))
+	# 2019年5月1日12時10分
+	judge_datetime = datetime.datetime(2019, 5, 1, 12, 10)
+	# 成田空港
+	point = (35.764702, 140.386002)
+	cnt = plane.density(judge_datetime, point)
 	end = time.time()
 	print(end-mid, "seconds")
 	print(cnt)
